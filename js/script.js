cc6887c455aee7b03d4462c85508fca5fbf7f0a9
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-12-05 19:18:05 -0300

    Atualizando

--- js/script.js
+++ js/script.js
@@ -1,61 +1,50 @@
-//criei uma variavel chamada elemento
+//criei uma variavel chamada elemento 
 //document acessa o documento html que esta sendo chamado o script
-//query selector e um methodo interno do javaScript que nos retorna um elemento html
-var elemento = document.querySelector("h1");
+//query selector e um methodo interno do javaScript que nos retorna um elemento html 
+var elemento = document.querySelector('h1')
 //variavel recebendo inner html para incremento ou alteracao descrtivo
-elemento.innerHTML += " JS";
-//variavel elemento recebendo estilizacao de cor
-elemento.style.color = "#white";
+elemento.innerHTML += ' JS';
+//variavel elemento recebendo estilizacao de cor 
+elemento.style.color = '#white';
 //console log e o nosso print retorno variavel
 console.log(elemento);
 
-//criei uma variavel chamada limpar
-function limpar(event) {
-  //preventDefault nao atualiza o reload da pagina
-  event.preventDefault();
-  //acessando documento html buscando elemento form e resetando formulario
-  document.querySelector("form").reset();
-
-  //console log limpando
-  console.log("Limpando....");
-}
-
 function limpar(event){
-  event.preventDefault();
-  document.querySelector('form').reset();
-  console.log('Limpando....');
+    event.preventDefault();
+    document.querySelector('form').reset();
+    console.log('Limpando....');
 }
 
 function salvar(event){
-  event.preventDefault();
-  console.log('salvando....');
-  
-  var name = document.getElementsByName('nome_completo')[0].value;
-  var cargo = document.getElementsByName('cargo')[0].value;
-  var salario = document.getElementsByName('salario')[0].value;
+    event.preventDefault();
+    console.log('salvando....');
+    
+    var name = document.getElementsByName('nome_completo')[0].value;
+    var cargo = document.getElementsByName('cargo')[0].value;
+    var salario = document.getElementsByName('salario')[0].value;
 
-  var listaFunc = JSON.parse(localStorage.getItem('Funcionarios'));
+    var listaFunc = JSON.parse(localStorage.getItem('Funcionarios'));
 
-  if(listaFunc == null){
-      listaFunc = [];
-  }
-  var id = JSON.parse(localStorage.getItem('IdFuncionario'));
-  if(listaFunc == null){
-      id = 0;
-  }
-  id = id +1;
+    if(listaFunc == null){
+        listaFunc = [];
+    }
+    var id = JSON.parse(localStorage.getItem('IdFuncionario'));
+    if(listaFunc == null){
+        id = 0;
+    }
+    id = id +1;
 
-  var Func = {
-                  'id': id,
-                  'nome_completo': name,
-                  'cargo': cargo,
-                  'salario': salario
-      };listaFunc.push(Func);
+    var Aluno = {
+                    'id': id,
+                    'nome_completo': name,
+                    'cargo': cargo,
+                    'salario': salario
+        };listaFunc.push(Aluno);
 
-  
-  localStorage.setItem('IdFuncionario', JSON.stringify(id));    
-  localStorage.setItem('Funcionarios', JSON.stringify(listaFunc));
-  limpar(event)
+    
+    localStorage.setItem('IdFuncionario', JSON.stringify(id));    
+    localStorage.setItem('Funcionarios', JSON.stringify(listaFunc));
+    // limpar(event)
 }
 
 document.getElementById('salvar').addEventListener('click', salvar)
